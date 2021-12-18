b6298751b5cf83e2740b700298cdbc7a3140f84d
vanderson nogueira <vanderson.01@hotmail.com>
2021-12-18 09:46:11 -0300

    initial

---
+++ src/main/resources/db/migration/V1_.sql
@@ -0,0 +1,3 @@
+insert into category (name , description) values ('Lazer' , 'Cinema, parques, viagens, etc...');
+insert into category (name , description) values ('Trabalho' , 'Freelancer empresa xpto.com');
+insert into category (name , description) values ('Contas' , 'Água, luz, condôminio');
