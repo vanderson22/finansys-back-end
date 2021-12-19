f9f468b5905f21427e5612c53e1ee44d17c484c9
vanderson nogueira <vanderson.01@hotmail.com>
2021-12-19 11:42:03 -0300

    [criar] Criar categoria

--- src/main/java/br/com/home/finansys/controller/CategoriaControlador.java
+++ src/main/java/br/com/home/finansys/controller/CategoriaControlador.java
@@ -2,11 +2,16 @@ package br.com.home.finansys.controller;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,9 +42,21 @@ public class CategoriaControlador {
 
 	}
 
+	@PostMapping
+	public ResponseEntity<Category> criar(@RequestBody(required = true) Category category) {
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.create(category));
+	}
+
 	@DeleteMapping(path = "{id}")
 	public void delete(@PathVariable(name = "id") Long id) {
 		service.delete(id);
+	}
 
+	@GetMapping(path = "{id}")
+	public ResponseEntity<Category> findById(@PathVariable(name = "id") Long id) {
+
+		Category category = service.findById(id);
+		return ResponseEntity.ok().body(category);
 	}
 }
--- src/main/java/br/com/home/finansys/service/CategoryService.java
+++ src/main/java/br/com/home/finansys/service/CategoryService.java
@@ -1,6 +1,7 @@
 package br.com.home.finansys.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,36 @@ public class CategoryService {
 
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public Category findById(Long id) {
+
+        Optional<Category> opt = repository.findById(id);
+        return opt.orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+    }
+
+    public Category create(Category category) {
+        validateCategory(category);
+        return repository.save(category);
+    }
+
+    /**
+     * 
+     * Realiza validação da categoria 
+     * 
+    */
+    private void validateCategory(Category category) {
+        if (category == null)
+            throw new RuntimeException("Categoria não pode ser nula");
+        if (category.getName().equals("") || category.getName() == null)
+            throw new RuntimeException("O nome da cateogria não pode ser nula");
+            
+        Optional<Category> opt = repository.findCategoryByName(category.getName());
+        if (opt.isPresent() && category.getId() == null )
+            throw new RuntimeException("Categoria já existe");
+            // se presente e os id's forem diferentes, não inserir
+        if (opt.isPresent()   && ( category.getId() != null &&  !category.getId().equals(opt.get().getId())) )
+            throw new RuntimeException("O identificador já existe para outra categoria");
     }
 
 }
